088905af36ed0eef658b6c813611dce8c1c40bb4
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-18 21:27:38 -0300

    Add Sequelize integration to CoursesModule
    Imported SequelizeModule and configured it with the Courses model. Added CoursesRepository to the providers array to support repository pattern.

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { CoursesController } from './courses.controller';
-import { CoursesService } from './courses.service';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { CoursesController } from "./courses.controller";
+import { CoursesService } from "./courses.service";
+import { CoursesRepository } from "./courses.repository";
+import { Courses } from "./courses.model";
 
 @Module({
+  imports: [SequelizeModule.forFeature([Courses])],
   controllers: [CoursesController],
-  providers: [CoursesService],
+  providers: [CoursesService, CoursesRepository],
 })
 export class CoursesModule {}
