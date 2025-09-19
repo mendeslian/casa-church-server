ce6bacaf62789e6e0dd5e54261c9a1f005c427fa
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-18 21:27:50 -0300

    Implement CRUD methods in CoursesService
    Added methods for creating, retrieving, updating, replacing, and deleting courses in the CoursesService. Integrated CoursesRepository for data access and added error handling for not found cases.

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CoursesRepository } from "./courses.repository";
+import { Courses } from "./courses.model";
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+  constructor(private readonly courseRepository: CoursesRepository) {}
+
+  async getAll(): Promise<Courses[] | null> {
+    return await this.courseRepository.findAll();
+  }
+
+  async getById(id: string): Promise<Courses | null> {
+    const student = await this.courseRepository.getById(id);
+
+    if (!student) throw new NotFoundException("Curso não encontrado");
+    return student;
+  }
+
+  async create(
+    course: Courses
+  ): Promise<{ message: string; newCourse: Courses }> {
+    const newCourse = await this.courseRepository.create(course);
+    return {
+      message: "Curso criado com sucesso",
+      newCourse,
+    };
+  }
+
+  async replace(
+    id: string,
+    newData: Courses
+  ): Promise<{ message: string; updatedCourse: Courses }> {
+    const course = await this.courseRepository.getById(id);
+
+    if (!course) throw new NotFoundException("Curso não encontrado");
+
+    const updatedCourse = await this.courseRepository.replace(id, newData);
+    return {
+      message: "Curso atualizado com sucesso",
+      updatedCourse,
+    };
+  }
+
+  async update(
+    id: string,
+    newData: Partial<Courses>
+  ): Promise<{ message: string; updatedCourse: Courses }> {
+    const course = await this.courseRepository.getById(id);
+
+    if (!course) throw new NotFoundException("Curso não encontrado");
+
+    const updatedCourse = await this.courseRepository.update(id, newData);
+    return {
+      message: "Curso atualizado com sucesso",
+      updatedCourse,
+    };
+  }
+
+  async delete(id: string): Promise<{ message: string }> {
+    const course = await this.courseRepository.getById(id);
+    if (!course) throw new NotFoundException("Curso não encontrado");
+
+    await this.courseRepository.delete(id);
+    return {
+      message: "Curso deletado com sucesso",
+    };
+  }
+}
