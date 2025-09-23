2fca6dde0bb5c5f362929cc09247df038246e3d1
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-22 21:03:16 -0300

    feat: creating user models

---
+++ src/users/entities/user.model.ts
@@ -0,0 +1,42 @@
+import { Default } from "sequelize-typescript";
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  PrimaryKey,
+  CreatedAt,
+  UpdatedAt,
+} from "sequelize-typescript";
+import { UserRoles } from "../types/user.types";
+
+@Table({ tableName: "users", timestamps: true })
+export class User extends Model {
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column(DataType.UUID)
+  declare id: string;
+
+  @Column({ type: DataType.STRING(150), allowNull: false })
+  declare name: string;
+
+  @Column({ type: DataType.STRING(100), allowNull: false, unique: true })
+  declare email: string;
+
+  @Column({ type: DataType.STRING(255), allowNull: false })
+  declare password: string;
+
+  @Default(UserRoles.USER)
+  @Column(DataType.ENUM(...Object.values(UserRoles)))
+  declare role: UserRoles;
+
+  @Default(true)
+  @Column(DataType.BOOLEAN)
+  declare active: boolean;
+
+  @CreatedAt
+  declare createdAt: Date;
+
+  @UpdatedAt
+  declare updatedAt: Date;
+}
