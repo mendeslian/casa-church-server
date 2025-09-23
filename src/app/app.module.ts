2088bccdf3edf7a39f38f70398ea820f331ac2c7
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-22 21:04:08 -0300

    feat adding UsersModule to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { StudentsModule } from 'src/students/students.module';
-import { CoursesModule } from 'src/courses/courses.module';
-import { DatabaseModule } from 'src/config/database.module';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { StudentsModule } from "src/students/students.module";
+import { CoursesModule } from "src/courses/courses.module";
+import { DatabaseModule } from "src/config/database.module";
+import { UsersModule } from "src/users/users.module";
 
 @Module({
-  imports: [DatabaseModule, StudentsModule, CoursesModule],
+  imports: [DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
