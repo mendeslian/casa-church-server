6f5ac9e0c6bc1b56e51926b833533d3d3dc9742b
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-09-28 18:16:46 -0300

    refactor: adjusting names of class validator not exposing API attributes

--- src/sermons/dto/create-sermon.dto.ts
+++ src/sermons/dto/create-sermon.dto.ts
@@ -1,14 +1,14 @@
 import { IsString, IsNotEmpty } from "class-validator";
 
 export class CreateSermonDto {
-    @IsNotEmpty({ message: "O titulo não pode estar vazio" })
-    @IsString({ message: "O titulo não está no formato adequado" })
-    declare title: string;
+  @IsNotEmpty({ message: "O título não pode estar vazio" })
+  @IsString({ message: "O título não está no formato adequado" })
+  declare title: string;
 
-    @IsString({ message: "A descricao não está no formato adequado" })
-    declare description: string
+  @IsString({ message: "A descrição não está no formato adequado" })
+  declare description: string;
 
-    @IsNotEmpty({ message: "O createdBy não pode estar vazio" })
-    @IsString({ message: "O createdBy não está no formato adequado" })
-    declare createdBy: string;
+  @IsNotEmpty({ message: "O usuário não pode estar vazio" })
+  @IsString({ message: "O usuário não está no formato adequado" })
+  declare createdBy: string;
 }
