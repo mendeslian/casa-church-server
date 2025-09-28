a0d4c052d67ea6d2d82f6f15c03db2d6953f8806
Lucas <lucascardoso2103@gmail.com>
2025-09-28 17:59:57 -0300

    feat: creating sermons module

---
+++ src/sermons/sermons.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from "@nestjs/sequelize";
+import { SermonsService } from './sermons.service';
+import { SermonsController } from './sermons.controller';
+import { SermonsRepository } from './sermons.repository';
+import { AuthModule } from "src/auth/auth.module";
+import { models } from 'src/models';
+
+@Module({
+  imports: [SequelizeModule.forFeature(models), AuthModule],
+  controllers: [SermonsController],
+  providers: [SermonsService, SermonsRepository],
+})
+export class SermonsModule { }
