baf1f29aaf82d6153b12e892d972d50188c79e68
Lucas <lucascardoso2103@gmail.com>
2025-09-28 21:18:51 -0300

    feat: importing Lesson module in models centralized file index

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,7 +3,8 @@ import { Event } from "src/events/entities/event.model";
 import { Registration } from "src/registrations/entities/registration.model";
 import { Post } from "src/posts/entities/post.model";
 import { Sermon } from "src/sermons/entities/sermon.model"
+import { Lesson } from "src/lessons/entities/lesson.model";
 
-export const models = [User, Event, Registration, Post, Sermon];
+export const models = [User, Event, Registration, Post, Sermon, Lesson];
 
-export { User, Event, Registration, Post, Sermon };
+export { User, Event, Registration, Post, Sermon, Lesson };
