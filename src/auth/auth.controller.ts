3339734d8b6b3ef244f960b7a4770179725f510e
lian mendes <132162402+mendeslian@users.noreply.github.com>
2025-10-13 22:30:01 -0300

    feat: adding swagger summary to login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
+import { ApiOperation } from "@nestjs/swagger";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: "Realizar login na plataforma",
+  })
   @Post("login")
   login(@Body() loginDto: LoginDto) {
     return this.authService.login(loginDto);
