7013b8dcbad4de540c3119e13da8ef600ade86a0
Hugo Fraga <hugoxfraga@hotmail.com>
2025-10-14 00:35:33 -0300

    [feat] saving success/error messages in constants and TokenPayLoad authenticator

--- src/donations/donations.controller.ts
+++ src/donations/donations.controller.ts
@@ -4,6 +4,8 @@ import { DonationsService } from './donations.service';
 import { CreateDonationDto } from './dto/create-donation.dto';
 import { UpdateDonationDto } from './dto/update-donation.dto';
 import { AuthTokenGuard } from "src/auth/guard/auth-token.guard";
+import { TokenPayloadParam } from "src/auth/params/token-payload.param";
+import { TokenPayloadDto } from "src/auth/dto/token-payload.dto";
 import { ApiOperation, ApiSecurity } from "@nestjs/swagger";
 
 @ApiSecurity("auth-token")
@@ -14,8 +16,8 @@ export class DonationsController {
 
   @ApiOperation({ summary: "Criar nova doação" })
   @Post()
-  create(@Body() createDonationDto: CreateDonationDto) {
-    return this.donationsService.create(createDonationDto);
+  create(@Body() createDonationDto: CreateDonationDto, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
+    return this.donationsService.create(createDonationDto, tokenPayload);
   }
 
   @ApiOperation({ summary: "Listar todas doações" })
@@ -26,25 +28,25 @@ export class DonationsController {
 
   @ApiOperation({ summary: "Listar doações por usuário" })
   @Get('user/:userId')
-  findAllByUserId(@Param('userId') userId: string) {
-    return this.donationsService.findAllByUserId(userId);
+  findAllByUserId(@Param('userId') userId: string, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
+    return this.donationsService.findAllByUserId(userId, tokenPayload);
   }
 
   @ApiOperation({ summary: "Visualizar detalhes de uma doação" })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.donationsService.findOne(id);
+  findOne(@Param('id') id: string, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
+    return this.donationsService.findOne(id, tokenPayload);
   }
 
   @ApiOperation({ summary: "Atualizar uma doação" })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDonationDto: UpdateDonationDto) {
-    return this.donationsService.update(id, updateDonationDto);
+  update(@Param('id') id: string, @Body() updateDonationDto: UpdateDonationDto, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
+    return this.donationsService.update(id, updateDonationDto, tokenPayload);
   }
 
   @ApiOperation({ summary: "Deletar uma doação" })
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.donationsService.remove(id);
+  remove(@Param('id') id: string, @TokenPayloadParam() tokenPayload: TokenPayloadDto) {
+    return this.donationsService.remove(id, tokenPayload);
   }
 }
--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,21 +1,28 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import { CREATED_DONATION, UPDATED_DONATION, DELETED_DONATION, NOT_FOUND_DONATION } from "./donation.constants";
 
+import { FORBIDDEN_OPERATION_MESSAGE } from "src/common/constants/messages.constants";
 import { CreateDonationDto } from './dto/create-donation.dto';
 import { UpdateDonationDto } from './dto/update-donation.dto';
 import { DonationsRepository } from './donations.repository';
+import { TokenPayloadDto } from "src/auth/dto/token-payload.dto";
+import { USER_ADMIN_ROLE } from "src/users/user.constants";
 
 @Injectable()
 export class DonationsService {
   constructor(private readonly donationsRepository: DonationsRepository) {}
 
-  async create(createDonationDto: CreateDonationDto) {
+  async create(createDonationDto: CreateDonationDto, tokenPayload: TokenPayloadDto) {
     const donationData = {
       ...createDonationDto,
     };
 
+    if (createDonationDto.userId !== tokenPayload.id)
+      throw new ForbiddenException(FORBIDDEN_OPERATION_MESSAGE);
+
     const donation = await this.donationsRepository.create(donationData);
     return {
-      message: 'Doação cadastrada com sucesso',
+      message: CREATED_DONATION,
       donation,
     };
   }
@@ -24,36 +31,66 @@ export class DonationsService {
     return await this.donationsRepository.findAll();
   }
 
-  async findAllByUserId(userId: string) {
+  async findAllByUserId(userId: string, tokenPayload: TokenPayloadDto) {
     const donation = await this.donationsRepository.findByUserId(userId);
+    if (!donation) throw new NotFoundException(NOT_FOUND_DONATION);
+    
+    if (
+      tokenPayload.role !== USER_ADMIN_ROLE &&
+      userId !== tokenPayload.id
+    ) {
+      throw new ForbiddenException(FORBIDDEN_OPERATION_MESSAGE);
+    }
 
-    if (!donation) throw new NotFoundException('Doação não encontrada');
     return donation;
   }
 
-  async findOne(id: string) {
+  async findOne(id: string, tokenPayload: TokenPayloadDto) {
     const donation = await this.donationsRepository.findById(id);
+    if (!donation) throw new NotFoundException(NOT_FOUND_DONATION);
+
+    if (
+      tokenPayload.role !== USER_ADMIN_ROLE &&
+      donation.userId !== tokenPayload.id
+    ) {
+      throw new ForbiddenException(FORBIDDEN_OPERATION_MESSAGE);
+    }
+
     return donation;
   }
 
-  async update(id: string, updateDonationDto: UpdateDonationDto) {
+  async update(id: string, updateDonationDto: UpdateDonationDto, tokenPayload: TokenPayloadDto) {
     const donation = await this.donationsRepository.findById(id);
-    if (!donation) throw new NotFoundException('Doação não encontrada');
+    if (!donation) throw new NotFoundException(NOT_FOUND_DONATION);
+
+    if (
+      tokenPayload.role !== USER_ADMIN_ROLE &&
+      donation.userId !== tokenPayload.id
+    ) {
+      throw new ForbiddenException(FORBIDDEN_OPERATION_MESSAGE);
+    }
 
     const updatedDonation = await this.donationsRepository.update(id, updateDonationDto);
     return {
-      message: 'Doação atualizada com sucesso',
+      message: UPDATED_DONATION,
       donation: updatedDonation,
     };
   }
 
-  async remove(id: string) {
+  async remove(id: string, tokenPayload: TokenPayloadDto) {
     const donation = await this.donationsRepository.findById(id);
-    if (!donation) throw new NotFoundException('Doação não encontrada');
+    if (!donation) throw new NotFoundException(NOT_FOUND_DONATION);
+
+    if (
+      tokenPayload.role !== USER_ADMIN_ROLE &&
+      donation.userId !== tokenPayload.id
+    ) {
+      throw new ForbiddenException(FORBIDDEN_OPERATION_MESSAGE);
+    }
 
     await this.donationsRepository.delete(id);
     return {
-      message: 'Doação deletada com sucesso',
+      message: DELETED_DONATION,
     };
   }
 }
